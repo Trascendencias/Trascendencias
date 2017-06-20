068faf91a2f1554e7e5ebf186e4bdecb083f69cc
EdgarMagdaleno <edgarmv97@gmail.com>
2017-06-20 12:29:12 -0700

    Update

--- server.js
+++ server.js
@@ -1,10 +1,18 @@
-const express = require('express')
-const app = express()
+const express = require('express');
+const app = express();
+const http_app = express();
 
-app.get('/', function (req, res) {
-  res.send('Hello World!')
-})
+app.engine('.html', require('ejs').__express);
+app.set('views', __dirname + '/views');
+app.set('view engine', 'html');
 
-app.listen(80, function () {
-  console.log('Listening on port 80');
-})
+http_app.get('*', function(request, response) {
+	response.redirect('https://edgarmagdaleno.me' + request.url);
+});
+
+app.get('/', function(req, res) {  
+	res.render('index', { title: 'The index page!' })
+});
+
+https.createServer(app).listen(443);
+http.createServer(http_app).listen(80);
